26130cb720ce5e310aca5e310062abed38347919
jaydenwu <jwu7916@gmail.com>
2025-12-12 15:56:19 -0500

    Initial commit

--- src/main/java/org/Wu/Address.java
+++ src/main/java/org/Wu/Address.java
@@ -15,20 +15,28 @@ public class Address {
     private String city;
     private Province province;
     private String postalCode;
-}
 
-public Address(int streetNo, String street, String city, Province province, String postalCode){
-    if (isPostalCodeValid(postalCode)) {
-        this.streetNo = streetNo;
-        this.street = street;
-        this.city = city;
-        this.province = province;
-        this.postalCode = postalCode.toUpperCase();
-    } else {
-        this.streetNo = 0;
-        this.street = null;
-        this.city = null;
-        this.province = null;
-        this.postalCode = null;
+    public Address(int streetNo, String street, String city, Province province, String postalCode) {
+        if (isPostalCodeValid(postalCode)) {
+            this.streetNo = streetNo;
+            this.street = street;
+            this.city = city;
+            this.province = province;
+            this.postalCode = postalCode.toUpperCase();
+        } else {
+            this.streetNo = 0;
+            this.street = null;
+            this.city = null;
+            this.province = null;
+            this.postalCode = null;
+        }
+    }
+
+    public static
+    public enum Gender {
+        MALE, FEMALE
+    }
+    public enum Province {
+        AB, BC, MB, NB, Nl, NS, NT, NU, ON, PE, QC, SK, YT
     }
 }
