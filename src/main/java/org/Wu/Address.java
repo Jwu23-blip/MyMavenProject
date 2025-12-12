c91c6ba680b63f3f4dc2794d73e7905c8501429a
jaydenwu <jwu7916@gmail.com>
2025-12-12 15:45:23 -0500

    Initial commit

--- src/main/java/org/Wu/Address.java
+++ src/main/java/org/Wu/Address.java
@@ -1,4 +1,18 @@
 package org.Wu;
 
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+@EqualsAndHashCode
 public class Address {
+    private int streetNo;
+    private String street;
+    private String city;
+    private Province province;
+    private String postalCode;
 }
